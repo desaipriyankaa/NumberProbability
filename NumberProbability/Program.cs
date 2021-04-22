7d7de2ef3576e4a88f232e445ec7253f7e9c9fc1
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-04-22 13:16:43 +0530

    Add project files.

---
+++ NumberProbability/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumberProbability
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 0, 1, 2 };
+            Console.WriteLine("numbers are : ");
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Possible number pairs are : ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                     Console.WriteLine(numbers[i]+","+numbers[j]);
+                }
+            }
+        }
+    }
+}
